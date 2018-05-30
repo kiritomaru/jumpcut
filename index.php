5f3c727c7a5fd0e5249acff3fdcca7ecb5621300
Kirito Maru <sanjomaru13@gmail.com>
2018-05-31 03:33:59 +0800

    Updates

--- index.php
+++ index.php
@@ -4,12 +4,56 @@
     <div class="wrap">
         <div class="col-md">
             <p class="blue-green free">FREE STEP-BY-STEP VIDEO SERIES REVEALS:</p>
-            <h1></h1>
+            <h1 class="toupper italic">How to Make More Money on YouTube</h1>
+            <h2 class="toupper light-gray italic">Than Most People Make from Their 9 to 5 Job</h2>
         </div>
         <div class="col-md">
-
+            <img src="assets/img/jumpvideo.jpg" alt="">
         </div>
         <div class="clear"></div>
+    </div>
+</div>
+
+<div id="results">
+    <div class="wrap text-center">
+        <h2 class="toupper italic">Results some of our students have gotten</h2>
+
+        <div class="students">
+            <div class="student-cont col-md">
+                <img src="assets/img/frugalaesthetic.jpg" alt="">
+                <h3 class="blue-green">YouTube channel:</h3>
+                <h4 class="toupper">Frugal Aesthetic</h4>
+                <div class="details">
+                    <p><strong>Date joined:</strong>  May 2016</p>
+                    <p><strong>Views gained:</strong>  +11.78 million</p>
+                    <p><strong>Subscribers gained:</strong>  +254,000</p>
+                    <p><strong> Job today:</strong> Full time on YouTube </p>
+                </div>
+            </div>
+             <div class="student-cont col-md">
+                <img src="assets/img/frugalaesthetic.jpg" alt="">
+                <h3 class="blue-green">YouTube channel:</h3>
+                <h4 class="toupper">Gumbino</h4>
+                <div class="details">
+                    <p><strong>Date joined:</strong>  May 2016</p>
+                    <p><strong>Views gained:</strong>  +11.78 million</p>
+                    <p><strong>Subscribers gained:</strong>  +254,000</p>
+                    <p><strong> Job today:</strong> Full time on YouTube </p>
+                </div>
+            </div>
+             <div class="student-cont col-md">
+                <img src="assets/img/frugalaesthetic.jpg" alt="">
+                <h3 class="blue-green">YouTube channel:</h3>
+                <h4 class="toupper">xoJahtna</h4>
+                <div class="details">
+                    <p><strong>Date joined:</strong>  May 2016</p>
+                    <p><strong>Views gained:</strong>  +11.78 million</p>
+                    <p><strong>Subscribers gained:</strong>  +254,000</p>
+                    <p><strong> Job today:</strong> Full time on YouTube </p>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
     </div>
 </div>
 
