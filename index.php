8ee601b53e2b1ca1036a91edc99544014e71bb0e
Kirito Maru <sanjomaru13@gmail.com>
2018-05-31 02:13:36 +0800

    Updated

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include_once('includes/header.php') ?>
+
+<div id="video-series">
+    <div class="wrap">
+        <div class="col-md">
+            <p class="blue-green free">FREE STEP-BY-STEP VIDEO SERIES REVEALS:</p>
+            <h1></h1>
+        </div>
+        <div class="col-md">
+
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+
+
+<?php include_once('includes/footer.php') ?>
